711140bfae4744633d1d159d46f4b5d5094ac551
Viktor <x.viktor.13@gmail.com>
2020-11-20 23:42:55 +0700

    Added a subscribe/unsubscribe button

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views import generic
 
 from .models import Blog, Post
@@ -6,9 +5,11 @@ from .models import Blog, Post
 
 class BlogListView(generic.ListView):
     model = Blog
+    context_object_name = 'blogs'
     template_name = 'blog/blog-list.html'
 
 
 class PostListView(generic.ListView):
     model = Post
+    context_object_name = 'posts'
     template_name = 'blog/post-list.html'
