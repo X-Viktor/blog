469d976d1abb73abc7881349b0c4b2a90b3b6e09
Viktor <x.viktor.13@gmail.com>
2020-11-20 22:53:40 +0700

    Added post output

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post-list.html'
